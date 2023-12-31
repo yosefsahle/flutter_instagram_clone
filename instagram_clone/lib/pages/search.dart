be0faeee2e825037dab1144039b7fd5ee4679384
yosefsahle <yosefsahle48@gmail.com>
2023-12-31 16:38:14 +0300

    Search Page

--- instagram_clone/lib/pages/search.dart
+++ instagram_clone/lib/pages/search.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:instagram_clone/util/explore_grid.dart';
 
 class UseSearch extends StatelessWidget {
   const UseSearch({super.key});
@@ -6,7 +7,25 @@ class UseSearch extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Center(child: Text('SEARCH')),
+      appBar: AppBar(
+          backgroundColor: Colors.transparent,
+          elevation: 0,
+          title: ClipRRect(
+            borderRadius: BorderRadius.circular(8),
+            child: Container(
+              padding: EdgeInsets.all(8),
+              color: Colors.grey[400],
+              child: Row(
+                children: [
+                  Icon(Icons.search),
+                  Container(
+                    child: Text('Search'),
+                  )
+                ],
+              ),
+            ),
+          )),
+      body: ExploreGrid(),
     );
   }
 }
