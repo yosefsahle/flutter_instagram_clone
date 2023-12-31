f9656b13c45e5f1e9f11d82d6a0c9cb2779205af
yosefsahle <yosefsahle48@gmail.com>
2023-12-31 16:53:25 +0300

    Shop Page

--- instagram_clone/lib/pages/shop.dart
+++ instagram_clone/lib/pages/shop.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:instagram_clone/util/shop_grid.dart';
 
 class UseShop extends StatelessWidget {
   const UseShop({super.key});
@@ -6,7 +7,51 @@ class UseShop extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Center(child: Text('SHOP')),
+      appBar: AppBar(
+        backgroundColor: Colors.transparent,
+        elevation: 0,
+        title: Padding(
+          padding: const EdgeInsets.all(8.0),
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              Text('Shop'),
+              Row(
+                children: [
+                  Icon(Icons.calendar_today),
+                  SizedBox(
+                    width: 10,
+                  ),
+                  Icon(Icons.menu)
+                ],
+              )
+            ],
+          ),
+        ),
+      ),
+      body: Column(
+        children: [
+          Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: ClipRRect(
+              borderRadius: BorderRadius.circular(8),
+              child: Container(
+                padding: EdgeInsets.all(8),
+                color: Colors.grey[400],
+                child: Row(
+                  children: [
+                    Icon(Icons.search),
+                    Container(
+                      child: Text('Search'),
+                    )
+                  ],
+                ),
+              ),
+            ),
+          ),
+          Expanded(child: ShopGrid())
+        ],
+      ),
     );
   }
 }
